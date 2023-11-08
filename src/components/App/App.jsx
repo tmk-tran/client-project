428c0b05f6e5429a6c72cab175cc55429be57ad7
T Mark <tmk1.tran@gmail.com>
2023-11-08 14:56:44 -0600

    added global MUI theme, set primary color to PSG brand blue

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
 import Nav from "../Nav/Nav";
 
@@ -21,6 +22,13 @@ import RegisterPage from "../RegisterPage/RegisterPage";
 
 import "./App.css";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#273b91", // Set your preferred global color here
+    },
+  },
+});
 
 function App() {
   const dispatch = useDispatch();
@@ -33,80 +41,82 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Nav />
-        <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+      <ThemeProvider theme={theme}>
+        <div>
+          <Nav />
+          <Switch>
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
+            {/* Visiting localhost:3000/about will show the about page. */}
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/about"
+            >
+              <AboutPage />
+            </Route>
 
-          {/* For protected routes, the view could show one of several things on the same route.
+            {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows UserPage else shows LoginPage
+              exact
+              path="/user"
+            >
+              <UserPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/org-input"
-          >
-            <NewOrgForm />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows InfoPage else shows LoginPage
+              exact
+              path="/org-input"
+            >
+              <NewOrgForm />
+            </ProtectedRoute>
 
-          <Route exact path="/login">
-            {user.id ? (
-              // If the user is already logged in,
-              // redirect to the /user page
-              <Redirect to="/user" />
-            ) : (
-              // Otherwise, show the login page
-              <LoginPage />
-            )}
-          </Route>
+            <Route exact path="/login">
+              {user.id ? (
+                // If the user is already logged in,
+                // redirect to the /user page
+                <Redirect to="/user" />
+              ) : (
+                // Otherwise, show the login page
+                <LoginPage />
+              )}
+            </Route>
 
-          <Route exact path="/registration">
-            {user.id ? (
-              // If the user is already logged in,
-              // redirect them to the /user page
-              <Redirect to="/user" />
-            ) : (
-              // Otherwise, show the registration page
-              <RegisterPage />
-            )}
-          </Route>
+            <Route exact path="/registration">
+              {user.id ? (
+                // If the user is already logged in,
+                // redirect them to the /user page
+                <Redirect to="/user" />
+              ) : (
+                // Otherwise, show the registration page
+                <RegisterPage />
+              )}
+            </Route>
 
-          <Route exact path="/home">
-            {user.id ? (
-              // If the user is already logged in,
-              // redirect them to the /user page
-              <Redirect to="/user" />
-            ) : (
-              // Otherwise, show the Landing page
-              <LandingPage />
-            )}
-          </Route>
+            <Route exact path="/home">
+              {user.id ? (
+                // If the user is already logged in,
+                // redirect them to the /user page
+                <Redirect to="/user" />
+              ) : (
+                // Otherwise, show the Landing page
+                <LandingPage />
+              )}
+            </Route>
 
-          {/* If none of the other routes matched, we will show a 404. */}
-          <Route>
-            <h1>404</h1>
-          </Route>
-        </Switch>
-      </div>
+            {/* If none of the other routes matched, we will show a 404. */}
+            <Route>
+              <h1>404</h1>
+            </Route>
+          </Switch>
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
