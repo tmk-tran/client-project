ea8f71a73a5c13f1e0cde9af7c167f9b64b0d1ce
jcolago <jcolago2005@gmail.com>
2023-11-09 16:22:10 -0600

    commit before merge

--- server/routes/group.details.router.js
+++ server/routes/group.details.router.js
@@ -53,4 +53,10 @@ router.put('/:id', (req, res) => {
     })
 })
 
+router.put('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    const queryText = `UPDATE "group" SET "is_deleted" = true WHERE "id" = $1`
+
+    pool.query(queryText, [id])
+})
 module.exports = router;
