317ee1bf277eec000137877b0b673a4fb9ca9d83
T Mark <tmk1.tran@gmail.com>
2023-11-09 13:40:42 -0600

    changed database name to psg_project, set up user and password

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,7 +28,7 @@ else {
     pool = new pg.Pool({
         host: 'localhost',
         port: 5432,
-        database: 'client-project',   // 	💥 Change this to the name of your database!
+        database: 'psg_project',   // 	💥 Change this to the name of your database!
     });
 }
 
