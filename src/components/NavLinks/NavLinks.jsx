646303fae83794a49cc22b779b23fd729c5d5ed8
T Mark <tmk1.tran@gmail.com>
2023-11-12 12:56:48 -0600

    styled NavLinks component with MUI, added underline textDecoration on hover, changed color of Links to PSG brand blue

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,39 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Link as MuiLink } from "@mui/material";
 import { useSelector } from "react-redux";
 // Style
-import { Typography } from "@mui/material";
+import { Typography, ThemeProvider, createTheme } from "@mui/material";
 import "./NavLinks.css";
+
+// Custom theme for MUI components
+const theme = createTheme({
+  typography: {
+    fontSize: 18,
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: '#273b91', // Customize link color
+          // Add other styles for MuiLink here
+        },
+      },
+    },
+  },
+});
 
 export default function NavLinks() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="NavLinks-container">
-      {/* If no user is logged in, show these links */}
-      {!user.id && (
-        // If there's no user, show login/registration links
-        <>
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-          <Link className="navLink" to="/about">
-            About
-          </Link>
-        </>
-      )}
+    <ThemeProvider theme={theme}>
+      <div className="NavLinks-container">
+        {/* If no user is logged in, show these links */}
+        {!user.id && (
+          // If there's no user, show login/registration links
+          <>
+            <Link className="navLink" to="/login">
+              Login / Register
+            </Link>
+            <Link className="navLink" to="/about">
+              About
+            </Link>
+          </>
+        )}
 
-      {/* If a user is logged in, show these links */}
-      {user.id && (
-        <>
-          <Link to="/home">
-            <Typography>Home</Typography>
-          </Link>
-          <li>Groups</li>
-          <li>Add New Fundraiser</li>
-          <Link to="/about">About</Link>
-        </>
-      )}
-    </div>
+        {/* If a user is logged in, show these links */}
+        {user.id && (
+          <>
+            <MuiLink
+              component={Link}
+              className="main-navlink"
+              to="/home"
+              underline="hover"
+            >
+              <Typography>Home</Typography>
+            </MuiLink>
+            <Typography>Groups</Typography>
+            <Typography>Add New Fundraiser</Typography>
+            <MuiLink
+              component={Link}
+              className="main-navlink"
+              to="/about"
+              underline="hover"
+            >
+              <Typography>About</Typography>
+            </MuiLink>
+          </>
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
