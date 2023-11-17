185866ce597e3fa06fb0d793ed2f4ed2cd2be1aa
jcolago <jcolago2005@gmail.com>
2023-11-17 10:46:03 -0600

    Styled group card and modal add fundraiser form

--- src/components/GroupDetailsCard/GroupDetailsCard.jsx
+++ src/components/GroupDetailsCard/GroupDetailsCard.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { Box, Button, Card, CardMedia, Modal, Typography, FormControl, OutlinedInput, InputLabel, TextField, Select, MenuItem } from "@mui/material";
-import  {style}  from "../Utils/helpers";
+import { Box, Button, Card, CardMedia, Modal, Typography, FormControl, OutlinedInput, InputLabel, TextField, Select, MenuItem, Grid } from "@mui/material";
+import { style } from "../Utils/helpers";
 
 
 export default function GroupDetailsCard({ group }) {
@@ -19,14 +19,14 @@ export default function GroupDetailsCard({ group }) {
    const [photoUrl, setPhotoUrl] = useState("")
    const [booksRequested, setBooksRequested] = useState("")
    const [booksCheckedOut, setBooksCheckedOut] = useState("")
-   const [booksOutValue, setBooksOutValue] = useState(0)
-   const [booksCheckedIn, setBooksCheckedIn] = useState(0)
-   const [booksSold, setBooksSold] = useState(0)
-   const [moneyReceived, setMoneyRecieved] = useState(0)
+   const [booksOutValue, setBooksOutValue] = useState("")
+   const [booksCheckedIn, setBooksCheckedIn] = useState("")
+   const [booksSold, setBooksSold] = useState("")
+   const [moneyReceived, setMoneyRecieved] = useState("")
    const [startDtate, setStartDate] = useState("")
    const [endDate, setEndDate] = useState("")
    const [couponBookId, setCouponBookId] = useState("")
-   const [outstandingBalance, setOutstandingBalance] = useState(0)
+   const [outstandingBalance, setOutstandingBalance] = useState("")
 
    const submitFundraiser = (e) => {
       e.preventDefault;
@@ -49,100 +49,138 @@ export default function GroupDetailsCard({ group }) {
       setOpen(false)
    }
 
-   const style = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      width: 600,
-      bgcolor: 'background.paper',
-      border: '2px solid #000',
-      boxShadow: 24,
-      p: 4,
-   };
+   
+  const style = {
+   position: "absolute",
+   top: "50%",
+   left: "50%",
+   transform: "translate(-50%, -50%)",
+   width: 600,
+   bgcolor: "background.paper",
+   border: "2px solid #000",
+   boxShadow: 24,
+   p: 4,
+ };
 
 
    return (
-      <Card style={{display: "flex", flexDirection: "column", padding: "10px", width: "600px", margin: "auto"}}>
-         <CardMedia style={{ objectFit: "cover", width: "400px", height:"400px", margin: "auto" }}
+      <>
+      <Card elevation={6} style={{ display: "flex", flexDirection: "column", padding: "10px", width: "600px", margin: "auto" }}>
+         <CardMedia style={{ objectFit: "cover", width: "400px", height: "400px", margin: "auto" }}
             className="cardMedia"
             component="img"
             image={group.group_photo} />
-            <div style={{margin: "auto"}}>
-         <Typography variant="h5">{group.department} {group.sub_department}</Typography>
-         <Typography>{group.group_description}</Typography>
+         <div style={{ margin: "auto" }}>
+            <Typography variant="h5">{group.department} {group.sub_department}</Typography>
+            <Typography>{group.group_description}</Typography>
          </div>
          <div style={{
             margin: "auto "
-      }}>
-         <Button variant="outlined" onClick={() => history.goBack()}>Back</Button> <Button variant="outlined" onClick={handleOpen}>Add Fundraiser</Button>
+         }}>
+            <Button variant="outlined" onClick={() => history.goBack()}>Back</Button> <Button variant="outlined" onClick={handleOpen}>Add Fundraiser</Button>
          </div>
+         </Card>
          <Modal open={open} onClose={handleClose} aria-labelledby="title"
             aria-describedby="form">
-            <Box sx={style}>
-               <Typography>Add A Fundraiser</Typography>
+            <Box sx={style} >
+               <Typography style={{textAlign: "center"}} variant="h5">Add A Fundraiser</Typography>
+               
                <form onSubmit={submitFundraiser}>
-                  <FormControl>
-                     <InputLabel htmlFor="title">Title</InputLabel>
-                     <OutlinedInput label="Title" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="description">Description</InputLabel>
-                     <OutlinedInput label="Description" type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="photo">Photo URL</InputLabel>
-                     <OutlinedInput lable="Photo URL" type="text" placeholder="Photo URL" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="books_requested">Books Requested</InputLabel>
-                     <OutlinedInput label="Books Requested" type="text" placeholder="Books Requested" value={booksRequested} onChange={(e) => setBooksRequested(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="books_checked_out">Books Checked Out</InputLabel>
-                     <OutlinedInput label="Books Checked Out" type="number" placeholder="Books Checked Out" value={booksCheckedOut} onChange={(e) => setBooksCheckedOut(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="books_out_value">Books Out Value</InputLabel>
-                     <OutlinedInput label="Books Out Value" type="number" placeholder="Books Out Value" value={booksOutValue} onChange={(e) => setBooksOutValue(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="books_checked_in">Books Checked In</InputLabel>
-                     <OutlinedInput label="Books Checked In" type="number" placeholder="Books Checked In" value={booksCheckedIn} onChange={(e) => setBooksCheckedIn(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="books_sole">Books Sold</InputLabel>
-                     <OutlinedInput label="Books Sold" type="number" placeholder="Books Sold" value={booksSold} onChange={(e) => setBooksSold(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="money_received">Money Received</InputLabel>
-                     <OutlinedInput label="Money Received" type="number" placeholder="Money Received" value={moneyReceived} onChange={(e) => setMoneyRecieved(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="start_date"></InputLabel>
-                     <TextField type="date" value={startDtate} onChange={(e) => setStartDate(e.target.value)}></TextField>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel></InputLabel>
-                     <TextField type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}></TextField>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="coupon-book">Select A Coupon Book Year</InputLabel>
-                     <Select style={{ width: "250px" }} label="Select A Coupon Book Year" value={couponBookId} onChange={(e) => setCouponBookId(e.target.value)}>
-                        <MenuItem value="" disabled>Please Select A Coupon Book Year</MenuItem>
-                        {couponBooks.map(couponBook => {
-                           return (<MenuItem value={couponBook.id} key={couponBook.id}>{couponBook.year}</MenuItem>)
-                        })}
-                     </Select>
-                  </FormControl>
-                  <FormControl>
-                     <InputLabel htmlFor="outstanding_balance">Outstanding Balance</InputLabel>
-                     <OutlinedInput lable="Outstanding Balance" type="number" placeholder="Outstanding Balance" value={outstandingBalance} onChange={(e) => setOutstandingBalance(e.target.value)}></OutlinedInput>
-                  </FormControl>
-                  <Button type="submit">Submit</Button>
+             
+                  <div>
+                  <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                     <Grid item xs={6}>
+                        <TextField style={{ margin: "5px" }} fullWidth label="Title" type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}></TextField>
+                     </Grid>
+                  
+                  
+                     <Grid item xs={6}>
+                        <TextField style={{ margin: "5px" }} fullWidth label="Description" type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}>
+                        </TextField>
+                     </Grid>
+                     </Grid>
+                  
+                  </div>
+                  <div>
+                  <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                     <Grid item xs={6}>
+                        <TextField fullWidth style={{ margin: "5px" }} label="Photo URL" type="text" placeholder="Photo URL" value={photoUrl} onChange={(e) => setPhotoUrl(e.target.value)}></TextField>
+                     </Grid>
+                  
+                  
+                     <Grid item xs={6}>
+                        <TextField fullWidth style={{ margin: "5px" }} label="Books Requested" type="text" placeholder="Books Requested" value={booksRequested} onChange={(e) => setBooksRequested(e.target.value)}></TextField>
+                     </Grid>
+                     </Grid>
+                  
+                  </div>
+                  <div>
+                  <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                     <Grid item xs={6}>
+                  <TextField fullWidth style={{ margin: "5px" }} label="Books Checked Out" type="number" placeholder="Books Checked Out" value={booksCheckedOut} onChange={(e) => setBooksCheckedOut(e.target.value)}></TextField>
+                  </Grid>
+                  
+                  
+                     <Grid item xs={6}>
+                        <TextField fullWidth style={{ margin: "5px" }} label="Books Out Value" type="number" placeholder="Books Out Value" value={booksOutValue} onChange={(e) => setBooksOutValue(e.target.value)}></TextField>
+                     </Grid>
+                  </Grid>
+                  </div>
+                  <div>
+                  <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                     <Grid item xs={6}>
+                        <TextField fullWidth style={{ margin: "5px" }} label="Books Checked In" type="number" placeholder="Books Checked In" value={booksCheckedIn} onChange={(e) => setBooksCheckedIn(e.target.value)}></TextField>
+                     </Grid>
+                  
+                  
+                  
+                     <Grid item xs={6}>
+                        <TextField fullWidth style={{ margin: "5px" }} label="Books Sold" type="number" placeholder="Books Sold" value={booksSold} onChange={(e) => setBooksSold(e.target.value)}></TextField>
+                     </Grid>
+                  </Grid>
+                  </div>
+                  <div>
+                  <Grid container spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                     <Grid item xs={4}>
+                        <InputLabel>Money Received</InputLabel>
+                        <TextField style={{ margin: "5px", marginRight: "0" }}  type="number" placeholder="Money Received" value={moneyReceived} onChange={(e) => setMoneyRecieved(e.target.value)}></TextField>
+                     </Grid> 
+                     <Grid item xs={4}>
+                        <InputLabel>Start Date</InputLabel>
+                        <TextField style={{ margin: "5px", marginLeft: "0" }} type="date" value={startDtate} onChange={(e) => setStartDate(e.target.value)}></TextField>
+                     </Grid>
+                     <Grid item xs={4}>
+                        <InputLabel>End Date</InputLabel>
+                        <TextField style={{ margin: "5px" }} type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)}></TextField>
+                     </Grid>
+                  </Grid>
+                  </div>
+                  <div>
+                  <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                     <Grid item xs={6}>
+                        <InputLabel htmlFor="coupon-book">Coupon Book Year</InputLabel>
+                        <Select fullWidth style={{ margin: "5px" }} label="Select A Coupon Book Year" value={couponBookId} onChange={(e) => setCouponBookId(e.target.value)}>
+                           <MenuItem  value="" disabled>Please Select A Coupon Book Year</MenuItem>
+                           {couponBooks.map(couponBook => {
+                              return (<MenuItem value={couponBook.id} key={couponBook.id}>{couponBook.year}</MenuItem>)
+                           })}
+                        </Select>
+                     </Grid>
+                 
+                  
+                     <Grid item xs={6}>
+                        <InputLabel>Outstanding Balance</InputLabel>
+                        <TextField fullWidth style={{ margin: "5px" }} type="number" placeholder="Outstanding Balance" value={outstandingBalance} onChange={(e) => setOutstandingBalance(e.target.value)}></TextField>
+                     </Grid>
+                     </Grid>
+                  
+                  </div>
+                  
+                  <Button type="submit" variant="contained" style={{margin: "5px"}}>Submit</Button>
                </form>
+               
             </Box>
          </Modal>
-      </Card>
+         </>
    )
 }
