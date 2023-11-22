95b89cbb97a3b352e93b74eb0c3922ccc94522d0
T Mark <tmk1.tran@gmail.com>
2023-11-22 17:56:31 -0600

    added clear notes textfield on save note

--- src/components/OrgNotesModal/OrgNotesModal.jsx
+++ src/components/OrgNotesModal/OrgNotesModal.jsx
@@ -72,6 +72,7 @@ export default function NotesPopover({ info, onNoteAdded }) {
     // Sweet Alert
     showSaveSweetAlert(saveCall);
 
+    setNewNote("");
     handleClose();
     onNoteAdded();
   };
