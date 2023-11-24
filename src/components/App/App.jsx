e498d9808e0b2fa257345b372ebd0465bb6ae689
T Mark <tmk1.tran@gmail.com>
2023-11-24 11:34:13 -0600

    removed landing page, redirected to Login page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -156,7 +156,7 @@ function App() {
                 <Redirect to="/user" />
               ) : (
                 // Otherwise, show the Landing page
-                <LandingPage />
+                <LoginPage />
               )}
             </Route>
 
