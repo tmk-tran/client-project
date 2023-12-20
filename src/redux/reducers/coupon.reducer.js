e4f380a54ace157231a68de2cac68be1f0f0ab04
T Mark <tmk1.tran@gmail.com>
2023-12-20 16:02:21 -0600

    removed unused lines in coupon.reducer

--- src/redux/reducers/coupon.reducer.js
+++ src/redux/reducers/coupon.reducer.js
@@ -8,31 +8,3 @@ const couponReducer = (state = [], action) => {
 };
 
 export default couponReducer;
-
-// // reducer.js
-// const initialState = {
-//     files: [],
-//     error: null,
-//   };
-  
-//   const merchantReducer = (state = initialState, action) => {
-//     switch (action.type) {
-//       case 'SET_MERCHANT_FILES':
-//         return {
-//           ...state,
-//           files: action.payload,
-//           error: null,
-//         };
-//       case 'FETCH_MERCHANT_FILES_FAILURE':
-//         return {
-//           ...state,
-//           error: action.payload,
-//         };
-//       // Add other reducer cases as needed
-//       default:
-//         return state;
-//     }
-//   };
-  
-//   export default merchantReducer;
-  
