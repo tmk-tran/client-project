d522eb8283cb27009dc38de2cbbe1a5b299baa4f
jcolago <jcolago2005@gmail.com>
2024-01-17 13:38:42 -0600

    Group details router updated for Devii api calls

--- server/routes/group.details.router.js
+++ server/routes/group.details.router.js
@@ -3,7 +3,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 //Get route for fundraisers and group details
-router.get('/:id', (req, res) => {
+router.post('/:id', (req, res) => {
     const id = req.params.id
     const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzUxMiJ9.eyJpYXQiOjE3MDU0MzA2NjMsIm5iZiI6MTcwNTQzMDY2MywianRpIjoiYzA3ZWNlMGEtNjdmYS00NjBiLThmOGQtZjc3M2NlNDk5OWY2IiwiZXhwIjoxNzA1NTE3MDYzLCJzdWIiOnsicm9sZWlkIjoyMDMzNiwidGVuYW50aWQiOjEwMTIxfSwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.Af36SYvSr6U3MO7sIRQKYlK9vf1xrphIsIdt50e7nz7oI2LEFLA42Q9MyiL0tN84YjfMYPNmkm3j7gPgnAlGkLO6ANBj4h9UVSdTYXqElXD3TkRiJ4Gduf_J2wQCpEhewL7oBzBotxT-3BIFszGluwujCSW7afoQUH6YkpjVEIP0KaP6";
     const QUERY_URL = "https://api.devii.io/query";
@@ -116,7 +116,7 @@ router.post('/', async (req, res) => {
   });
 });
   
-router.post("/", (req, res) => {
+router.post("/newgroup", (req, res) => {
   const newGroup = req.body;
   //May need to ask if this format is correct for creating stuff
   const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzUxMiJ9.eyJpYXQiOjE3MDU0MzA2NjMsIm5iZiI6MTcwNTQzMDY2MywianRpIjoiYzA3ZWNlMGEtNjdmYS00NjBiLThmOGQtZjc3M2NlNDk5OWY2IiwiZXhwIjoxNzA1NTE3MDYzLCJzdWIiOnsicm9sZWlkIjoyMDMzNiwidGVuYW50aWQiOjEwMTIxfSwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.Af36SYvSr6U3MO7sIRQKYlK9vf1xrphIsIdt50e7nz7oI2LEFLA42Q9MyiL0tN84YjfMYPNmkm3j7gPgnAlGkLO6ANBj4h9UVSdTYXqElXD3TkRiJ4Gduf_J2wQCpEhewL7oBzBotxT-3BIFszGluwujCSW7afoQUH6YkpjVEIP0KaP6";
@@ -164,6 +164,36 @@ router.post("/", (req, res) => {
 // });
 //PUT route to update group department/sub-department, title, nickname, photo, and description
 
+router.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedGroup = req.body;
+  const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzUxMiJ9.eyJpYXQiOjE3MDU0MzA2NjMsIm5iZiI6MTcwNTQzMDY2MywianRpIjoiYzA3ZWNlMGEtNjdmYS00NjBiLThmOGQtZjc3M2NlNDk5OWY2IiwiZXhwIjoxNzA1NTE3MDYzLCJzdWIiOnsicm9sZWlkIjoyMDMzNiwidGVuYW50aWQiOjEwMTIxfSwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.Af36SYvSr6U3MO7sIRQKYlK9vf1xrphIsIdt50e7nz7oI2LEFLA42Q9MyiL0tN84YjfMYPNmkm3j7gPgnAlGkLO6ANBj4h9UVSdTYXqElXD3TkRiJ4Gduf_J2wQCpEhewL7oBzBotxT-3BIFszGluwujCSW7afoQUH6YkpjVEIP0KaP6";
+  const QUERY_URL = "https://api.devii.io/query";
+  const query = `{\r\n  mutation{\r\n create_group(\r\n input: {\r\n organization_id: ${updatedGroup.organization_id}\r\n department: ${updatedGroup.department}\r\n sub_department: ${updatedGroup.sub_department}\r\n group_nickname: ${updatedGroup.group_nickname}\r\n group_photo: ${updatedGroup.group_photo}\r\n group_description: ${updatedGroup.group_description}\r\n is_deleted: ${updatedGroup.is_deleted}\r\n}\r\n id: ${id}\r\n \r\n) {\r\n id\r\n organization_id\r\n department\r\n sub_department\r\n group_nickname\r\n group_photo\r\n group_description\r\n is_deleted}}\r\n}`;
+  
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${ACCESS_TOKEN}`);
+  myHeaders.append("Content-Type", "application/json");
+  
+  var graphql = JSON.stringify({
+    query: query,
+    variables: {},
+  });
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: graphql,
+    redirect: "follow",
+  };
+  
+  fetch(QUERY_URL, requestOptions)
+    .then((response) => response.text())
+     .then((result) => {console.log(result);
+    res.sendStatus(200)})
+    .catch((error) => {console.log("Error getting data from Devii", error)
+    res.sendStatus(500)
+  });
+})
 
 //Original update route
 // router.put('/:id', (req, res) => {
@@ -180,20 +210,22 @@ router.post("/", (req, res) => {
 //         res.sendStatus(500);
 //     })
 // })
-//PUT route to update the soft delete for a group
-router.put('/delete/:id', (req, res) => {
-    const id = req.params.id;
-    const queryText = `UPDATE "group" SET "is_deleted" = 'true' WHERE "id" = $1`
 
-    pool.query(queryText, [id])
-    .then(() => {
-        res.sendStatus(200)
-    })
-    .catch((err) => {
-        console.log("Error in marking group as deleted", err);
-        res.sendStatus(500)
-    })
-})
+//Origional put route to update is_deleted column
+//PUT route to update the soft delete for a group
+// router.put('/delete/:id', (req, res) => {
+//     const id = req.params.id;
+//     const queryText = `UPDATE "group" SET "is_deleted" = 'true' WHERE "id" = $1`
+
+//     pool.query(queryText, [id])
+//     .then(() => {
+//         res.sendStatus(200)
+//     })
+//     .catch((err) => {
+//         console.log("Error in marking group as deleted", err);
+//         res.sendStatus(500)
+//     })
+// })
 
 
 module.exports = router;
