29745b98f892b0516b21cd1c9d23290384a15c68
T Mark <tmk1.tran@gmail.com>
2024-01-21 03:10:37 -0600

    New Task creation form select fields added, options related to task management addded

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,29 +1,9 @@
-// import * as React from "react";
-// import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-
-
-// export default function BasicDatePicker() {
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <DemoContainer components={["DatePicker"]}>
-//         <DatePicker
-//           label="Due Date"
-//         />
-//       </DemoContainer>
-//     </LocalizationProvider>
-//   );
-// }
-
-
 import * as React from "react";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import TextField from "@mui/material/TextField";
+
 
 export default function BasicDatePicker() {
   return (
@@ -31,11 +11,31 @@ export default function BasicDatePicker() {
       <DemoContainer components={["DatePicker"]}>
         <DatePicker
           label="Due Date"
-          TextFieldComponent={(props) => (
-            <TextField {...props} sx={{ width: "500px" }} />
-          )}
         />
       </DemoContainer>
     </LocalizationProvider>
   );
 }
+
+
+// import * as React from "react";
+// import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+// import TextField from "@mui/material/TextField";
+
+// export default function BasicDatePicker() {
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <DemoContainer components={["DatePicker"]}>
+//         <DatePicker
+//           label="Due Date"
+//           TextFieldComponent={(props) => (
+//             <TextField {...props} />
+//           )}
+//         />
+//       </DemoContainer>
+//     </LocalizationProvider>
+//   );
+// }
--- src/components/NewTaskModal/NewTaskModal.jsx
+++ src/components/NewTaskModal/NewTaskModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // ~~~~~~~~~~ Style ~~~~~~~~~~
 import {
   Box,
@@ -25,10 +25,45 @@ const style = {
   p: 4,
 };
 
+const taskOptions = {
+  Contact: ["Initial Contact", "Follow Up Call/Email/Text", "Follow Up Visit"],
+  Book: ["Drop Off Books", "Organization Picking Up Books"],
+  Task: [
+    "Organization Dropping Books Off",
+    "Received Payment",
+    "Send Invoice",
+    "Other",
+  ],
+};
+
 export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+
+  const [firstMenuChoice, setFirstMenuChoice] = useState("");
+  const [secondMenuChoice, setSecondMenuChoice] = useState("");
+  const [thirdMenuChoice, setThirdMenuChoice] = useState("");
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleFirstMenuChange = (event) => {
+    const choice = event.target.value;
+    setFirstMenuChoice(choice);
+    setSecondMenuChoice(""); // Reset second menu choice when first menu changes
+    setThirdMenuChoice(""); // Reset third menu choice when first menu changes
+  };
+
+  useEffect(() => {
+    // Reset second menu choice when first menu changes
+    setSecondMenuChoice("");
+  }, [firstMenuChoice]);
+
+  useEffect(() => {
+    // Reset third menu choice when second menu changes
+    if (!taskOptions[secondMenuChoice]?.includes(thirdMenuChoice)) {
+      setThirdMenuChoice("");
+    }
+  }, [secondMenuChoice, thirdMenuChoice]);
 
   return (
     <div>
@@ -47,24 +82,50 @@ export default function BasicModal() {
           </Typography>
 
           <div style={{ display: "flex", flexDirection: "column" }}>
-            <Select sx={{ margin: "5px 0" }}>
-              <MenuItem>Book</MenuItem>
-              <MenuItem>Contact</MenuItem>
-              <MenuItem>Coupon</MenuItem>
-              <MenuItem>Task</MenuItem>
+            <Select
+              value={firstMenuChoice}
+              onChange={handleFirstMenuChange}
+              sx={{ margin: "5px 0" }}
+            >
+              <MenuItem value="">Select...</MenuItem>
+              <MenuItem value="Book">Book</MenuItem>
+              <MenuItem value="Contact">Contact</MenuItem>
+              <MenuItem value="Task">Task</MenuItem>
             </Select>
-            <Select sx={{ margin: "5px 0" }}>
-              <MenuItem></MenuItem>
-            </Select>
-            <Select sx={{ margin: "5px 0" }}>
-              <MenuItem></MenuItem>
-            </Select>
-            <Select sx={{ margin: "5px 0" }}>
-              <MenuItem></MenuItem>
-            </Select>
-            <DatePicker />
-          </div>
 
+            <Select
+              value={secondMenuChoice}
+              onChange={(event) => {
+                setSecondMenuChoice(event.target.value);
+                setThirdMenuChoice(""); // Reset third menu when changing the second menu
+              }}
+              sx={{ margin: "5px 0" }}
+            >
+              <MenuItem value="">Select...</MenuItem>
+              {taskOptions[firstMenuChoice]?.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+
+            <Select
+              value={thirdMenuChoice}
+              onChange={(event) => setThirdMenuChoice(event.target.value)}
+              sx={{ margin: "5px 0" }}
+            >
+              <MenuItem value="">Select...</MenuItem>
+              {taskOptions[secondMenuChoice]?.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+
+            <div style={{ margin: "0 auto" }}>
+              <DatePicker />
+            </div>
+          </div>
           <TextField
             id="outlined-multiline-static"
             label="Additional Details"
