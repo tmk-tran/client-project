708b047523f66fe560848acb1bdf958b7bd80049
T Mark <tmk1.tran@gmail.com>
2024-01-24 11:30:40 -0600

    removed Card from DenyProofModal

--- src/components/DenyProofModal/DenyProofForm.jsx
+++ src/components/DenyProofModal/DenyProofForm.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 // ~~~~~~~~~~ Style ~~~~~~~~~~
 import {
-  Card,
-  CardContent,
   Typography,
   TextField,
   Button,
@@ -13,48 +11,46 @@ import { hoverDeny, hoverAccept } from "../Utils/helpers";
 
 const DenyProofForm = ({ onClose }) => {
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h6" align="center">
-          Please Enter Reason
-        </Typography>
+    <div>
+      <Typography variant="h6" align="center">
+        Please Enter Reason
+      </Typography>
 
-        <TextField
-          multiline
-          rows={4}
-          fullWidth
-          label="Comments or coupon changes here..."
-          variant="outlined"
-          style={{ marginTop: 16 }}
-        />
+      <TextField
+        multiline
+        rows={4}
+        fullWidth
+        label="Comments or coupon changes here..."
+        variant="outlined"
+        style={{ marginTop: 16 }}
+      />
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: 16,
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 16,
+        }}
+      >
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: errorColor.color, ...hoverDeny }}
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: successColor.color,
+            width: "105px",
+            ...hoverAccept,
           }}
         >
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: errorColor.color, ...hoverDeny }}
-            onClick={onClose}
-          >
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: successColor.color,
-              width: "105px",
-              ...hoverAccept,
-            }}
-          >
-            Ok
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+          Ok
+        </Button>
+      </div>
+    </div>
   );
 };
 
