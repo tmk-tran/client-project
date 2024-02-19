187d837c4bce7dce85fff0b7855d2e2fa7f40e02
T Mark <tmk1.tran@gmail.com>
2024-02-18 22:47:54 -0600

    removed unused lines from development in OrderSummaryTable

--- src/components/CheckoutPage/OrderSummaryTable.jsx
+++ src/components/CheckoutPage/OrderSummaryTable.jsx
@@ -1,64 +1,3 @@
-// import React, { useState } from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableRow,
-//   TextField,
-//   Paper,
-//   Typography,
-// } from "@mui/material";
-
-// export default function OrderSummaryTable({ selectedProducts }) {
-//   const [newQuantity, setNewQuantity] = useState(0);
-
-//   return (
-//     <Paper>
-//       <Table>
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Book Type</TableCell>
-//             <TableCell>Price</TableCell>
-//             <TableCell>Quantity</TableCell>
-//             <TableCell>Total</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {selectedProducts ? (
-//             selectedProducts.map((product, index) => (
-//               <TableRow key={index}>
-//                 <TableCell>{product.bookType}</TableCell>
-//                 <TableCell>${product.price}</TableCell>
-//                 {/* <TableCell>{product.quantity}</TableCell> */}
-//                 <TableCell>
-//                   <TextField
-//                     type="number"
-//                     value={newQuantity}
-//                     onChange={(e) => setNewQuantity(e.target.value)}
-//                     InputProps={{ inputProps: { min: 1 } }}
-//                     sx={{ width: "20%" }}
-//                   />
-//                 </TableCell>
-//                 <TableCell>${product.price * product.quantity}</TableCell>
-//               </TableRow>
-//             ))
-//           ) : (
-//             <TableRow>
-//               <TableCell colSpan={4}>
-//                 <Typography variant="h6" gutterBottom>
-//                   No products selected.
-//                 </Typography>
-//               </TableCell>
-//             </TableRow>
-//           )}
-//         </TableBody>
-//       </Table>
-//     </Paper>
-//   );
-// }
-
-// OrderSummaryTable.js
 import React, { useState } from "react";
 import {
   Table,
@@ -102,12 +41,14 @@ export default function OrderSummaryTable({
     if (productId === 4) {
       setCustomDonation(newQuantity);
     } else {
-      const updatedQuantities = { ...localQuantities, [productId]: newQuantity };
+      const updatedQuantities = {
+        ...localQuantities,
+        [productId]: newQuantity,
+      };
       setLocalQuantities(updatedQuantities);
       onUpdateQuantity(updatedQuantities);
     }
   };
-  
 
   return (
     <Paper>
@@ -133,13 +74,12 @@ export default function OrderSummaryTable({
                     `$ ${product.price}`
                   )}
                 </TableCell>
-                <TableCell align="right" sx={{ width: "8%" }}>
+                <TableCell align="right" sx={{ width: "10%" }}>
                   {product.bookType === "Donate" ? (
                     <TextField
                       type="number"
                       value={customDonation}
                       onChange={(e) =>
-                        // onUpdateQuantity({ [product.id]: e.target.value })
                         handleQuantityChange(product.id, e.target.value)
                       }
                       InputProps={{ inputProps: { min: 1 } }}
