ea124559c4c9421db5d044a6da7e6a4c2ef6d7c4
T Mark <tmk1.tran@gmail.com>
2024-02-21 15:11:23 -0600

    successful EDIT for updating cash amount for sellerPage

--- server/routes/sellerPage.router.js
+++ server/routes/sellerPage.router.js
@@ -3,10 +3,10 @@ const pool = require("../modules/pool");
 const router = express.Router();
 
 router.get("/:refId", (req, res) => {
-    console.log("from GET /id seller.router: ", req.params.refId);
-    const refId = req.params.refId;
-  
-    const queryText = `
+  console.log("from GET /id seller.router: ", req.params.refId);
+  const refId = req.params.refId;
+
+  const queryText = `
       SELECT
         *
       FROM
@@ -14,17 +14,44 @@ router.get("/:refId", (req, res) => {
       WHERE
         "refId" = $1    
       ;`;
-  
-    pool
-      .query(queryText, [refId])
-      .then((result) => {
-        console.log("from GET /id seller.router: ", result.rows);
-        res.send(result.rows);
-      })
-      .catch((err) => {
-        console.log("error in the GET / request for sellers", err);
-        res.sendStatus(500);
-      });
-  });
+
+  pool
+    .query(queryText, [refId])
+    .then((result) => {
+      console.log("from GET /id seller.router: ", result.rows);
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("error in the GET / request for sellers", err);
+      res.sendStatus(500);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const sellerInfo = req.body;
+  const refId = req.params.id;
+  console.log("Req.body from sellers = ", sellerInfo);
+
+  const queryText = `
+          UPDATE 
+            "sellers"
+          SET
+            "cash" = $1
+          WHERE 
+            "refId" = $2;`;
+
+  const values = [sellerInfo.cash, refId];
+
+  pool
+    .query(queryText, values)
+    .then((response) => {
+      console.log("response from PUT sellerPage.router: ", response.rows);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("error with sellerPage PUT route", err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
