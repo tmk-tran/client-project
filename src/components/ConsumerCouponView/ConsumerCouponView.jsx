46201887d44f31da5e627899ba2de00a8cd4e5c9
T Mark <tmk1.tran@gmail.com>
2024-02-27 19:04:44 -0600

    layout for coupon cards set, ready to pull in data to fill appropriate sections

--- src/components/ConsumerCouponView/ConsumerCouponView.jsx
+++ src/components/ConsumerCouponView/ConsumerCouponView.jsx
@@ -1,21 +1,30 @@
 import { Box, Grid, Typography } from "@mui/material";
-import CouponList from "./CouponList";
+import CouponCard from "./CouponCard";
 import { border } from "../Utils/colors";
 
 export default function ConsumerCouponView() {
   return (
-    <Box sx={border}>
-      <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "center" }}>Consumer Coupon View</Typography>
+    <Box>
+      <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "center" }}>
+        Consumer Coupon View
+      </Typography>
+      <br />
       <Grid container spacing={2}>
-      <Grid item xs={6}>
-        {/* First column */}
-        <CouponList />
+        <Grid item xs={6}>
+          {/* First column */}
+          <CouponCard />
+          <CouponCard />
+          <CouponCard />
+          <CouponCard />
+        </Grid>
+        <Grid item xs={6}>
+          {/* Second column */}
+          <CouponCard />
+          <CouponCard />
+          <CouponCard />
+          <CouponCard />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        {/* Second column */}
-        <CouponList />
-      </Grid>
-    </Grid>
     </Box>
   );
 }
---
+++ src/components/ConsumerCouponView/CouponCard.jsx
@@ -0,0 +1,38 @@
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import { border } from "../Utils/colors";
+import { centerDiv } from "../Utils/helpers";
+
+const couponPreviewStyle = {
+  height: "80px",
+  width: "200px",
+};
+
+export default function CouponCard() {
+  return (
+    <Card elevation={3} sx={{ mb: 2 }}>
+      <CardContent>
+        <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+          <Box sx={{ ...couponPreviewStyle, ...centerDiv, ...border }}>
+            Front
+          </Box>
+          <Box sx={{ ...couponPreviewStyle, ...centerDiv, ...border }}>
+            Back
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexGrow: 1,
+              ...border,
+            }}
+          >
+            <Typography variant="body2">Coupon details here</Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center", ml: "auto" }}>
+            <Button variant="outlined">Redeem</Button>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
