8a245adcfae7ce711540c8a83d746d6f3de30c87
Joseph Colago <137341653+jcolago@users.noreply.github.com>
2024-03-01 16:07:33 -0600

    Update allUsers.router.js

--- server/routes/allUsers.router.js
+++ server/routes/allUsers.router.js
@@ -5,70 +5,70 @@ const router = express.Router();
 
 
 //Origional get route for users
-// router.get("/", (req, res) => {
-//   pool
-//     .query(
-//       `SELECT
-//       u.id AS user_id,
-//       u.username,
-//       jsonb_agg(
-//           JSONB_BUILD_OBJECT(
-//               'id', ug.id,
-//               'group_id', ug.group_id,
-//               'group_nickname', g.group_nickname,
-//               'group_department', g.department,
-//               'group_subdepartment', g.sub_department,
-//               'organization_name', o.organization_name,
-//               'group_admin', ug.group_admin
-//           )
-//       ) AS groups
-//   FROM
-//       "user" u
-//   LEFT JOIN
-//       "user-group" ug ON u.id = ug.user_id
-//   LEFT JOIN
-//       "group" g ON ug.group_id = g.id
-//   LEFT JOIN
-//       "organization" o ON g.organization_id = o.id
-//   WHERE
-//       u.is_deleted = false
-//   GROUP BY
-//       u.id, u.username
-//   ORDER BY
-//       u.id ASC;`
-//     )
-//     .then((response) => {
-//       res.send(response.rows).status(200);
-//     })
-//     .catch((error) => {
-//       console.log("Error in all users GET route", error);
-//       res.sendStatus(500);
-//     });
-// });
+router.get("/", (req, res) => {
+  pool
+    .query(
+      `SELECT
+      u.id AS user_id,
+      u.username,
+      jsonb_agg(
+          JSONB_BUILD_OBJECT(
+              'id', ug.id,
+              'group_id', ug.group_id,
+              'group_nickname', g.group_nickname,
+              'group_department', g.department,
+              'group_subdepartment', g.sub_department,
+              'organization_name', o.organization_name,
+              'group_admin', ug.group_admin
+          )
+      ) AS groups
+  FROM
+      "user" u
+  LEFT JOIN
+      "user-group" ug ON u.id = ug.user_id
+  LEFT JOIN
+      "group" g ON ug.group_id = g.id
+  LEFT JOIN
+      "organization" o ON g.organization_id = o.id
+  WHERE
+      u.is_deleted = false
+  GROUP BY
+      u.id, u.username
+  ORDER BY
+      u.id ASC;`
+    )
+    .then((response) => {
+      res.send(response.rows).status(200);
+    })
+    .catch((error) => {
+      console.log("Error in all users GET route", error);
+      res.sendStatus(500);
+    });
+});
 
 //Might need to ask about this and how the logic works on fronnt end
 
 
 //Origional put route to add to user groups table
-// router.put('/', (req, res) => {
-//     const groups = req.body.groups; // Access the groups array
+router.put('/', (req, res) => {
+    const groups = req.body.groups; // Access the groups array
 
-//     const updatePromises = groups.map((group) => {
-//       const queryText = 'UPDATE "user-group" SET "group_admin" = $1 WHERE "id" = $2;';
+    const updatePromises = groups.map((group) => {
+      const queryText = 'UPDATE "user-group" SET "group_admin" = $1 WHERE "id" = $2;';
 
-//       return pool.query(queryText, [group.group_admin, group.id]);
-//     });
+      return pool.query(queryText, [group.group_admin, group.id]);
+    });
 
-//     // Wait for all update queries to complete
-//     Promise.all(updatePromises)
-//       .then(() => {
-//         res.sendStatus(200);
-//       })
-//       .catch((err) => {
-//         console.log("Error in marking group admin", err);
-//         res.sendStatus(500);
-//       });
-//   });
+    // Wait for all update queries to complete
+    Promise.all(updatePromises)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log("Error in marking group admin", err);
+        res.sendStatus(500);
+      });
+  });
 
 
 module.exports = router;
