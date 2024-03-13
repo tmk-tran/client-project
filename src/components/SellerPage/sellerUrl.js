db422eab049eef09833e9b4edb644654160d0fd3
T Mark <tmk1.tran@gmail.com>
2024-03-13 11:01:35 -0500

    updated fly.toml file

--- src/components/SellerPage/sellerUrl.js
+++ src/components/SellerPage/sellerUrl.js
@@ -1,16 +1,16 @@
-export const transactionsUrl = (refId) => {
-  return `localhost:3000/#/seller/${refId}/cash`;
-};
-
-export const orderUrl = (refId) => {
-  return `localhost:3000/#/seller/${refId}`;
-};
-
-// FOR TEST ENVIRONMENT USE ~~~~~~~~~~~~~~~~ //
 // export const transactionsUrl = (refId) => {
-//   return `testpsg.fly.dev/#/seller/${refId}/cash`;
+//   return `localhost:3000/#/seller/${refId}/cash`;
 // };
 
 // export const orderUrl = (refId) => {
-//   return `testpsg.fly.dev/#/seller/${refId}`;
+//   return `localhost:3000/#/seller/${refId}`;
 // };
+
+// FOR TEST ENVIRONMENT USE ~~~~~~~~~~~~~~~~ //
+export const transactionsUrl = (refId) => {
+  return `testpsg.fly.dev/#/seller/${refId}/cash`;
+};
+
+export const orderUrl = (refId) => {
+  return `testpsg.fly.dev/#/seller/${refId}`;
+};
