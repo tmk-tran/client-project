4d9255a5e843e198fd99ecf46771d413745e5fbf
T Mark <tmk1.tran@gmail.com>
2024-03-13 22:50:14 -0500

    reformat lines in root saga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -26,13 +26,6 @@ import customersSaga from "./customers.saga";
 import transactionsSaga from "./transactions.saga";
 import redeemSaga from "./couponRedeem.saga";
 
-// rootSaga is the primary saga.
-// It bundles up all of the other sagas so our project can use them.
-// This is imported in index.js as rootSaga
-
-// some sagas trigger other sagas, as an example
-// the registration triggers a login
-// and login triggers setting the user
 export default function* rootSaga() {
   yield all([
     loginSaga(), // login saga is now registered
