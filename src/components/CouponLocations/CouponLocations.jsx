85c0edc3a2793510e10924c5c9e742025a45ac90
T Mark <tmk1.tran@gmail.com>
2024-03-22 12:28:26 -0500

    updated styling in CouponReviewDetails, added typography for headers

--- src/components/CouponLocations/CouponLocations.jsx
+++ src/components/CouponLocations/CouponLocations.jsx
@@ -17,55 +17,84 @@ const bold = {
 };
 
 const CouponLocations = ({ locations }) => {
+  console.log(locations);
+  if (
+    !locations ||
+    locations.length === 0 ||
+    locations.some(
+      (location) =>
+        location.location_name === null ||
+        location.phone_number === null ||
+        location.address === null ||
+        location.city === null ||
+        location.state === null ||
+        location.zip === null
+    )
+  ) {
+    return (
+      <>
+        <Typography label="Locations Accepted:" variant="h6" />
+        <br />
+        <Typography label="No locations set" sx={{ mb: 2, ml: 4 }} />
+      </>
+    );
+  }
+
   return (
     <div>
-      <h2>Accepted Locations:</h2>
-      {locations.length > 0 ? (
-        <ol>
-          {locations.map((location, index) => (
-            <li key={index}>
-              <Typography
-                label="Location Name: "
-                variant="body2"
-                sx={{ ...textStyle, ...bold }}
-              />
-              <Typography
-                label={capitalizeWords(location.location_name)}
-                variant="body2"
-                sx={textStyle}
-              />
-              <br />
-              <Typography
-                label="Phone: "
-                variant="body2"
-                sx={{ ...textStyle, ...bold }}
-              />
-              <Typography
-                label={formatPhoneNumber(location.phone_number)}
-                variant="body2"
-                sx={textStyle}
-              />
-              <br />
-              <Typography
-                label="Address: "
-                variant="body2"
-                sx={{ ...textStyle, ...bold }}
-              />
-              <Typography
-                label={`${capitalizeWords(location.address)} ${capitalizeWords(
-                  location.city
-                )}, ${capitalizeStateAbbr(location.state)} ${location.zip}`}
-                variant="body2"
-                sx={textStyle}
-              />
-              <br />
-              <br />
-            </li>
-          ))}
-        </ol>
-      ) : (
-        <p>No locations set</p>
-      )}
+      <Typography label="Accepted Locations:" variant="h6" />
+      <ol>
+        {locations.map((location, index) => (
+          <li key={index}>
+            <Typography
+              label="Location Name: "
+              variant="body2"
+              sx={{ ...textStyle, ...bold }}
+            />
+            <Typography
+              label={
+                location.location_name
+                  ? capitalizeWords(location.location_name)
+                  : ""
+              }
+              variant="body2"
+              sx={textStyle}
+            />
+            <br />
+            <Typography
+              label="Phone: "
+              variant="body2"
+              sx={{ ...textStyle, ...bold }}
+            />
+            <Typography
+              label={
+                location.phone_number
+                  ? formatPhoneNumber(location.phone_number)
+                  : ""
+              }
+              variant="body2"
+              sx={textStyle}
+            />
+            <br />
+            <Typography
+              label="Address: "
+              variant="body2"
+              sx={{ ...textStyle, ...bold }}
+            />
+            <Typography
+              label={`${
+                location.address ? capitalizeWords(location.address) + " " : ""
+              }${location.city ? capitalizeWords(location.city) + ", " : ""}${
+                location.state ? capitalizeStateAbbr(location.state) + " " : ""
+              }${location.zip || ""}`}
+              variant="body2"
+              sx={textStyle}
+            />
+            <br />
+            <br />
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
