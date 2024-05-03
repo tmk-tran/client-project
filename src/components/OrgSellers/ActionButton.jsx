4221fc064d7e3f5d090a24a972f585b9427e5636
T Mark <tmk1.tran@gmail.com>
2024-05-03 11:07:32 -0500

    resolved MUI error in UserAdmin for ToolTip

--- src/components/OrgSellers/ActionButton.jsx
+++ src/components/OrgSellers/ActionButton.jsx
@@ -14,33 +14,33 @@ const ActionButton = ({
   disabled,
 }) => {
   return (
-    <Tooltip title={title}>
-      <IconButton
-        aria-label="launch"
-        onClick={onClick}
-        onMouseOver={onMouseOver}
-        onMouseOut={onMouseOut}
-        sx={{
-          // fontSize: "larger",
-          height: "25px",
-          width: "25px",
-          color: primaryColor.color,
-          "&:hover": {
-            color: "#325CAB",
-            transition: "transform 0.2s",
-            cursor: "pointer",
-          },
-          ...(disabled && {
-            opacity: 0.5, // Gray out the button when disabled
-            pointerEvents: "none", // Disable pointer events
-          }),
-          ...buttonSx,
-        }}
-        disabled={disabled}
-      >
+    <IconButton
+      aria-label="launch"
+      onClick={onClick}
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      sx={{
+        // fontSize: "larger",
+        height: "25px",
+        width: "25px",
+        color: primaryColor.color,
+        "&:hover": {
+          color: "#325CAB",
+          transition: "transform 0.2s",
+          cursor: "pointer",
+        },
+        ...(disabled && {
+          opacity: 0.5, // Gray out the button when disabled
+          pointerEvents: "none", // Disable pointer events
+        }),
+        ...buttonSx,
+      }}
+      disabled={disabled}
+    >
+      <Tooltip title={title}>
         <Icon sx={iconSx} />
-      </IconButton>
-    </Tooltip>
+      </Tooltip>
+    </IconButton>
   );
 };
 
