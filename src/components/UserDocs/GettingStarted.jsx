98ede6f13bb2a1884113e9116fbcabe3dfe67e94
T Mark <tmk1.tran@gmail.com>
2024-06-20 16:19:06 -0500

    updated user docs page, layout set for side menu

--- src/components/UserDocs/GettingStarted.jsx
+++ src/components/UserDocs/GettingStarted.jsx
@@ -1,12 +1,59 @@
 import { Typography } from "@mui/material";
 
+// Sets margin bottom to 2
+const CustomTypography = (props) => {
+  return <Typography {...props} sx={{ mb: 2 }} />;
+};
+
 export default function GettingStarted() {
   return (
     <>
-      <Typography variant="h4">Getting Started</Typography>
-      <Typography variant="body1">
-        This is the getting started section...
-      </Typography>
+      <CustomTypography variant="h4">
+        Welcome to The Preferred Savings Guide!
+      </CustomTypography>
+      <CustomTypography variant="body1">
+        This application is designed to enhance the existing platform developed
+        in the Koss cohort at Emerging Digital Academy. Our goal is to provide a
+        comprehensive solution for coupon development, management, and sales.
+      </CustomTypography>
+      <CustomTypography variant="body1">Key Objectives:</CustomTypography>
+      <ul>
+        <li>
+          <Typography variant="body2">
+            <strong>Coupon Development:</strong> Create, manage, and publish
+            coupons from initial design to consumer availability.
+          </Typography>
+        </li>
+        <li>
+          <Typography variant="body2">
+            <strong>Task Tracking System:</strong> Manage tasks, communicate
+            with admin, and track progress.
+          </Typography>
+        </li>
+        <li>
+          <Typography variant="body2">
+            <strong>Seller Organization:</strong> Organize sellers, track
+            metrics, and manage sales data.
+          </Typography>
+        </li>
+        <li>
+          <Typography variant="body2">
+            <strong>Unique Seller URLs:</strong> Generate unique web links for
+            sellers to sell their coupon books.
+          </Typography>
+        </li>
+        <li>
+          <Typography variant="body2">
+            <strong>PayPal Integration:</strong> Integrate PayPal for secure
+            transactions within the application.
+          </Typography>
+        </li>
+      </ul>
+      <CustomTypography variant="body1">
+        This project aims to streamline coupon management, improve seller
+        organization, and enhance the user experience for administrators and
+        sellers alike.
+      </CustomTypography>
     </>
   );
 }
--- src/components/UserDocs/SideMenu.jsx
+++ src/components/UserDocs/SideMenu.jsx
@@ -1,24 +1,54 @@
+import React, { useState } from "react";
 import {
   Box,
+  Collapse,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
   Typography,
 } from "@mui/material";
+import { KeyboardArrowDown, KeyboardArrowRight } from "@mui/icons-material";
+
+const IndentedListItemText = (props) => {
+  return <ListItemText {...props} sx={{ ml: 2 }} />;
+};
 
 export default function SideMenu() {
+  const [isVideoTutorialsOpen, setIsVideoTutorialsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsVideoTutorialsOpen(!isVideoTutorialsOpen);
+  };
   // Table of Contents
   return (
     <Box sx={{ minWidth: 200, padding: 2 }}>
-      <Typography variant="h6">Table of Contents</Typography>
       <List>
         <ListItemButton>
-          <ListItemText primary="Getting Started" />
+          <ListItemText primary="Introduction" />
         </ListItemButton>
-        <ListItemButton>
-          <ListItemText primary="FAQs" />
+        <ListItemButton onClick={handleToggle}>
+          <ListItemText primary="Video Tutorials" />
+          {isVideoTutorialsOpen ? (
+            <KeyboardArrowDown />
+          ) : (
+            <KeyboardArrowRight />
+          )}
         </ListItemButton>
+        <Collapse in={isVideoTutorialsOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton>
+              <IndentedListItemText primary="Video 1" />
+            </ListItemButton>
+            <ListItemButton>
+              <IndentedListItemText primary="Video 2" />
+            </ListItemButton>
+            <ListItemButton>
+              <IndentedListItemText primary="Video 3" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+
         <ListItemButton>
           <ListItemText primary="Troubleshooting" />
         </ListItemButton>
