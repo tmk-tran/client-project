796b426773981823b6b95f14d3d7cc978dcee01a
T Mark <tmk1.tran@gmail.com>
2024-07-01 14:08:30 -0500

    reformat code in TableGroupDetails

--- src/components/TableGroupDetails/TableGroupDetails.jsx
+++ src/components/TableGroupDetails/TableGroupDetails.jsx
@@ -20,12 +20,19 @@ export default function TableGroupDetails({
       <TableHead>
         <TableRow>
           <TableCell>
-            <Typography variant="body1" style={centerStyle} sx={{ mb: 1, mt: 2, fontWeight: "bold" }}>
+            <Typography
+              variant="body1"
+              style={centerStyle}
+              sx={{ mb: 1, mt: 2, fontWeight: "bold" }}
+            >
               Total Goal:
             </Typography>
           </TableCell>
           <TableCell>
-            <Typography variant="body1" sx={{ fontSize: "25px", fontWeight: "bold" }}>
+            <Typography
+              variant="body1"
+              sx={{ fontSize: "25px", fontWeight: "bold" }}
+            >
               {totalGoals > 0 ? (
                 new Intl.NumberFormat("en-US", {
                   style: "currency",
@@ -56,7 +63,9 @@ export default function TableGroupDetails({
         </TableRow>
         <TableRow>
           <TableCell>
-            <Typography sx={{ ...centerStyle, mb: 2 }}>Fundraiser End Date(s):</Typography>
+            <Typography sx={{ ...centerStyle, mb: 2 }}>
+              Fundraiser End Date(s):
+            </Typography>
           </TableCell>
           <TableCell>
             {fundraiserInfo.map((fundraiser) =>
