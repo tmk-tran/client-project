071aa39526281bb709a7bd60b4659762c86ad229
T Mark <tmk1.tran@gmail.com>
2024-09-05 17:34:29 -0500

    Added waitingText to LoadingSpinner, redeploy

--- src/components/HomePage/LoadingSpinner.jsx
+++ src/components/HomePage/LoadingSpinner.jsx
@@ -3,17 +3,26 @@ import { Box, CircularProgress, Typography } from "@mui/material";
 // ~~~~~~~~~ Hooks ~~~~~~~~~~ //
 import { centeredStyle } from "../Utils/pageStyles";
 
-export default function LoadingSpinner({ text, waitingText, finalText, timeout, size }) {
+export default function LoadingSpinner({
+  text,
+  waitingText,
+  finalText,
+  timeout,
+  size,
+}) {
   const [displayedText, setDisplayedText] = useState(text);
   const [showWaitingText, setShowWaitingText] = useState(false);
 
   useEffect(() => {
     // Timer to show waitingText
-    const waitingTimer = setTimeout(() => {
-      if (waitingText) {
-        setShowWaitingText(true);
-      }
-    }, timeout ? timeout / 2 : 3000); // Show waitingText halfway through the timeout or default to 3000ms
+    const waitingTimer = setTimeout(
+      () => {
+        if (waitingText) {
+          setShowWaitingText(true);
+        }
+      },
+      timeout ? timeout / 2 : 3000
+    ); // Show waitingText halfway through the timeout or default to 3000ms
 
     // Timer to show finalText
     const finalTimer = setTimeout(() => {
@@ -30,6 +39,9 @@ export default function LoadingSpinner({ text, waitingText, finalText, timeout, 
   return (
     <Box sx={centeredStyle}>
       <CircularProgress size={size} />
+      {showWaitingText && !displayedText && (
+        <Typography variant="body2">{waitingText}</Typography>
+      )}
       {displayedText && (
         <Typography variant="body2">{displayedText}</Typography>
       )}
