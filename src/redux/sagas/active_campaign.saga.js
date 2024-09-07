9abab908b5c27dda8d618c963a6f8d99da70deef
T Mark <tmk1.tran@gmail.com>
2024-09-07 16:15:53 -0500

    PSG-211 users now added to user table after purchase

--- src/redux/sagas/active_campaign.saga.js
+++ src/redux/sagas/active_campaign.saga.js
@@ -54,26 +54,27 @@ function* addContactSaga(action) {
     //   console.log("No account creation necessary for physical book.");
     // }
     if (response.data === "OK") {
-      if (bookType === "Digital") {
-        console.log("Contact created successfully (no password returned), for a digital book.");
-    
-        // Only create an account for digital books
-        const user = {
-          username: action.payload.email,
-          password: response.data,
-          first_name: action.payload.firstName,
-          last_name: action.payload.lastName,
-        };
-    
-        yield put({ type: "REGISTER", payload: user });
-      } else {
-        // If it's a physical book, no account creation is necessary
-        console.log("No account creation necessary for physical book.");
-      }
-    } else {
-      console.log("No OK response, nor digital bookType");
-    }
+      // No account creation necessary for physical books
+      console.log(
+        "Contact created successfully (no account creation necessary)"
+      );
+    } else if (
+      bookType === "Fargo - Moorhead (Digital Coupon Book)" &&
+      typeof response.data === "string" &&
+      response.data.length > 0
+    ) {
+      // Create an account for digital books if valid data exists
+      const { email, firstName, lastName } = action.payload;
 
+      const user = {
+        username: email,
+        password: response.data,
+        first_name: firstName,
+        last_name: lastName,
+      };
+
+      yield put({ type: "REGISTER", payload: user });
+    }
   } catch (error) {
     console.log("error in addContact saga", error);
   }
